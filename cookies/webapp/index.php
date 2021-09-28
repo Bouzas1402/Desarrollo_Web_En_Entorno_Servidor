95f46bb70fe97e71f4c90590b8813f4b03010efa
Bouzas <carlos.bouzas@educa.madrid.or>
2021-09-28 16:25:49 +0200

    clase del 28/09/2021 a las 15:30

---
+++ cookies/webapp/index.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+<?php
+echo "Hola mundo, encantado de conocerte";
+setcookie("username", "carlos bouzas", time()+38*24*60*60);
+ ?>
+
+</body>
+</html>
+
---
+++ cookies/webapp/nuevacookies.php
@@ -0,0 +1,10 @@
+<?php
+
+    setcookie("username", "carlos alvaro", time()+38*24*60*60);
+
+?>
+
+
+
+
+
