72ce6ab29d8a229682ea28100ceb56f576f9a148
Bouzas <carlos.bouzas@educa.madrid.or>
2021-09-28 20:44:51 +0200

    hola

--- cookies/webapp/index.php
+++ cookies/webapp/index.php
@@ -4,6 +4,7 @@
     <title>Title</title>
 </head>
 <body>
+
 <?php
 echo "Hola mundo, encantado de conocerte";
 setcookie("username", "carlos bouzas", time()+38*24*60*60);
---
+++ cookies/webapp/usuarios/index2.php
@@ -0,0 +1,14 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Title</title>
+</head>
+<body>
+
+<?php
+echo "Hola mundo, encantado de conocerte";
+
+?>
+
+</body>
+</html>
